3e70c96cfb5432af10919f2ac793c4ee9e541e5d
Phat Menghor <phatmenghor19@gmail.com>
2026-01-18 21:00:39 +0700

    [development] Auto commit on 2026-01-18 21:00:37 | Files: c_git_pull.sh

--- c_git_pull.sh
+++ c_git_pull.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-echo "🚀 Pulling latest changes from origin/development..."
-git pull origin development
+# Get current date and time
+CURRENT_TIME=$(date "+%Y-%m-%d %H:%M:%S")
 
+# Get current branch name
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# Fetch remote updates
+git fetch
+
+# Pull latest code for the current branch
+git pull origin $BRANCH
+
+echo "✅ Auto pull completed from '$BRANCH' at $CURRENT_TIME"
