ae87ab4070aa2bf5da1b72a2dcaa864c27c0877b
Phat Menghor <menghor.phat@cambodiapostbank.com.kh>
2026-01-19 17:43:37 +0700

    [development] Auto commit on 2026-01-19 17:43:34 | Files: src/redux/features/hr/store/models/request/work-schedule-type.ts src/constants/status/type.ts src/redux/features/hr/store/models/response/work-schedule-response.ts "src/redux/features/hr/store/models/schema/work-schedule-type.schema

---
+++ src/redux/features/hr/store/models/schema/work-schedule-type.schema copy.ts
@@ -0,0 +1,63 @@
+import { DayOfWeek } from "@/types/business-profile";
+import { z } from "zod";
+
+export const createWorkScheduleSchema = z.object({
+  userId: z.string().uuid("Invalid userId"),
+  businessId: z.string().uuid("Invalid businessId"),
+  name: z.string().min(1, "Schedule name is required"),
+  scheduleTypeEnumName: z.string().min(1, "Schedule type is required"),
+  workDays: z
+    .array(z.enum(DayOfWeek))
+    .min(1, "At least one work day is required"),
+
+  startTime: z
+    .string()
+    .regex(/^\d{2}:\d{2}(:\d{2})?$/, "Invalid time format HH:mm or HH:mm:ss"),
+  endTime: z
+    .string()
+    .regex(/^\d{2}:\d{2}(:\d{2})?$/, "Invalid time format HH:mm or HH:mm:ss"),
+  breakStartTime: z
+    .string()
+    .regex(/^\d{2}:\d{2}(:\d{2})?$/, "Invalid time format HH:mm or HH:mm:ss")
+    .optional(),
+  breakEndTime: z
+    .string()
+    .regex(/^\d{2}:\d{2}(:\d{2})?$/, "Invalid time format HH:mm or HH:mm:ss")
+    .optional(),
+});
+
+export const updateWorkScheduleSchema = z.object({
+  name: z.string().min(1, "Schedule name is required"),
+  scheduleTypeEnumName: z.string().min(1, "Schedule type is required"),
+  workDays: z
+    .array(z.enum(DayOfWeek))
+    .min(1, "At least one work day is required"),
+
+  startTime: z
+    .string()
+    .regex(/^\d{2}:\d{2}(:\d{2})?$/, "Invalid time format HH:mm or HH:mm:ss"),
+  endTime: z
+    .string()
+    .regex(/^\d{2}:\d{2}(:\d{2})?$/, "Invalid time format HH:mm or HH:mm:ss"),
+  breakStartTime: z
+    .string()
+    .regex(/^\d{2}:\d{2}(:\d{2})?$/, "Invalid time format HH:mm or HH:mm:ss")
+    .optional(),
+  breakEndTime: z
+    .string()
+    .regex(/^\d{2}:\d{2}(:\d{2})?$/, "Invalid time format HH:mm or HH:mm:ss")
+    .optional(),
+});
+
+export type WorkScheduleTypeFormData = {
+  id: string;
+  userId: string;
+  businessId: string;
+  name: string;
+  scheduleTypeEnumName: string;
+  workDays: DayOfWeek[];
+  startTime: string;
+  endTime: string;
+  breakStartTime?: string;
+  breakEndTime?: string;
+};
