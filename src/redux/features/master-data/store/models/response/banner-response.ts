9fecc5b9b2a8a78dc17b5cd9d040fe3d61d83773
Phat Menghor <phatmenghor19@gmail.com>
2026-01-22 19:35:02 +0700

    [claude/redesign-ecommerce-cards-Um89O] Auto commit on 2026-01-22 19:35:02 | Files: package-lock.json package.json src/redux/features/main/components/home/banner-section.tsx src/redux/features/master-data/store/models/response/banner-response.ts yarn.lock

--- src/redux/features/master-data/store/models/response/banner-response.ts
+++ src/redux/features/master-data/store/models/response/banner-response.ts
@@ -11,6 +11,7 @@ export interface BannerResponseModel {
   createdBy: string;
   updatedBy: string;
   businessId: string;
+  description: string;
   businessName: string;
   imageUrl: string;
   linkUrl: string;
