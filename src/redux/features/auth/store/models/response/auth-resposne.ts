ebca1750d236499629eb9c9e0892f0e05b8b8559
Phat Menghor <menghor.phat@cambodiapostbank.com.kh>
2026-01-23 11:36:32 +0700

    [development] Auto commit on 2026-01-23 11:36:31 | Files: src/app/(auth)/login/page.tsx src/redux/features/auth/store/models/response/auth-resposne.ts

--- src/redux/features/auth/store/models/response/auth-resposne.ts
+++ src/redux/features/auth/store/models/response/auth-resposne.ts
@@ -4,14 +4,18 @@
  */
 
 export interface UserAuthResponseModel {
+  accessToken: string;
+  refreshToken: string;
+  tokenType: string;
   userId: string;
   userIdentifier: string;
   email: string;
   fullName: string;
-  profileImageUrl?: string;
+  profileImageUrl: any;
   userType: string;
-  businessId?: string;
   roles: string[];
-  accessToken: string;
-  refreshToken?: string;
+  businessId: string;
+  businessName: string;
+  businessStatus: string;
+  isSubscriptionActive: string;
 }
