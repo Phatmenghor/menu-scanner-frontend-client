ee928611264bfb2ba3a2caa1ef29ac4df4c2ca4c
Phat Menghor <menghor.phat@cambodiapostbank.com.kh>
2026-03-10 16:54:43 +0700

    [claude/fix-card-map-ui-habkt] Auto commit on 2026-03-10 16:54:42 | Files: src/constants/app-resource/default/default.ts

--- src/constants/app-resource/default/default.ts
+++ src/constants/app-resource/default/default.ts
@@ -1,7 +1,7 @@
 export const AppDefault = {
   RESET_PASSWORD: "88889999",
   // BUSINESS_ID: "85bbdf31-e034-45ad-8a20-71b7638667d2",
-  BUSINESS_ID: "e502ce0d-6f36-4390-ae79-7c54f7dd8c86",
+  BUSINESS_ID: "32bf7b6e-56fa-45d5-80fe-fa99282b3886",
   PAGE_SIZE: 15,
   PAGE_SIZE_OPTIONS: [10, 15, 20, 50, 100],
 };
